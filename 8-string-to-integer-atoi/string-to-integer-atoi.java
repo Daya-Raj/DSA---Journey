aec6e9c6ec5baeec176f2f8d1ea98a8ff947a8d3
Daya raj Yedla <itsdaya1718@gmail.com>
2026-06-27 08:48:28 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (75.36%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getNum(String s,int i,int sign,long result){
+        if(i>=s.length() || !Character.isDigit(s.charAt(i)))return sign*(int)result;
+        result = result * 10 + s.charAt(i)-'0';
+        if(sign*result >= Integer.MAX_VALUE)return Integer.MAX_VALUE;
+        if(sign*result <= Integer.MIN_VALUE)return Integer.MIN_VALUE;
+        return getNum(s,i+1,sign,result);
+    }
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s== null || s.isEmpty())return 0;
+        int i = 0,sign = 1;
+        if(s.charAt(i)=='-'|| s.charAt(i)=='+'){
+            sign = (s.charAt(i)=='-')?-1:1;
+            i++;
+        }
+        while(i<s.length() && s.charAt(i)=='0')i++;
+        return getNum(s,i,sign,0L);
+    }
+}
