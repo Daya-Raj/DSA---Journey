13cb747abc631a905c269f9f2e66645148d3e40b
Daya raj Yedla <itsdaya1718@gmail.com>
2026-08-01 08:37:52 +0530

    Time: 66 ms (23.33%) | Memory: 42.9 MB (41.06%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,22 @@
+
+class Solution {
+
+    private int solve(int[] nums,int left,int right){
+
+        if(left==right)
+            return nums[left];
+
+        int takeLeft =
+                nums[left]-solve(nums,left+1,right);
+
+        int takeRight =
+                nums[right]-solve(nums,left,right-1);
+
+        return Math.max(takeLeft,takeRight);
+    }
+
+    public boolean predictTheWinner(int[] nums){
+
+        return solve(nums,0,nums.length-1)>=0;
+    }
+}
