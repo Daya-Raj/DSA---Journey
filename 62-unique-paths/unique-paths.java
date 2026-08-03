abdd6192b911add0733ec6df31ad030b47b9b241
Daya raj Yedla <itsdaya1718@gmail.com>
2026-08-03 11:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.71%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,17 @@
 class Solution {
-    private int solve(int m,int n,int[][] dp){
-        if(m==0 && n==0)return 1;
-        if(m<0 || n<0)return 0;
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-        int up =solve(m-1,n,dp);
-        int down = solve(m,n-1,dp);
-        return dp[m][n] = up+down;
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int[] i : dp){
-            Arrays.fill(i,-1);
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow, 1);
+
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n];
+            Arrays.fill(currentRow, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
         }
-        return solve(m-1,n-1,dp);
+
+        return aboveRow[n - 1];        
     }
 }
