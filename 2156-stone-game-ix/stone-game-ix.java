3912d49a57d17b9587d954096c62db3828b1f3ff
Daya raj Yedla <itsdaya1718@gmail.com>
2026-08-16 11:55:44 +0530

    Time: 7 ms (29.05%) | Memory: 114.6 MB (94.59%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 =0;
+        int cnt1 = 0;
+        int cnt2=0;
+        for(int i:stones){
+            if(i%3==0){
+                cnt0++;
+            }
+            else if(i%3==1){
+                cnt1++;
+            }
+            else{
+                cnt2++;
+            }
+        }
+        if(cnt0%2==0){
+            return cnt2>0 && cnt1>0;
+        }
+        return Math.abs(cnt1-cnt2)>2;
+    }
+}
